494a4f207a36bb6d76b0fe97fab36dfe9b4e2b83
Romain TOUZE <romain.touze@gmail.com>
2015-12-17 11:13:30 +0100

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,40 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {HeroesComponent} from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {DashboardComponent} from './dashboard.component';
+import Map = require('esri/map');
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+    <router-outlet></router-outlet>
+  `,
+  styles: [`
+    a {padding: 5px;text-decoration: none;}
+    a:visited, a:link {color: #444;}
+    a:hover {color: white; background-color: #1171a3;}
+    a.router-link-active {color: white; background-color: #52b9e9;}
+  `],
+  directives: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+  // {path: '/', redirectTo: ['Dashboard'] },
+  {path: '/', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
+  {path: '/heroes', name: 'Heroes', component: HeroesComponent},
+  {path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent}
+])
+export class AppComponent {
+  public title = 'Tour of Heroes';
+
+  constructor() {
+    var map : Map = new Map("map", {
+      center: [-118, 34.5],
+      zoom: 8,
+      basemap: "topo"
+    });
+  }
+}
