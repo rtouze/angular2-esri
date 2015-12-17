3fea81a24bd3252013eba6a8e49343736f6e994a
Romain TOUZE <romain.touze@gmail.com>
2015-12-17 12:30:56 +0100

    c'est simple, ca marche

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,16 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {HeroesComponent} from './heroes.component';
-import {HeroDetailComponent} from './hero-detail.component';
-import {DashboardComponent} from './dashboard.component';
-import Map = require('esri/map');
+//import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+//import {HeroesComponent} from './heroes.component';
+//import {HeroDetailComponent} from './hero-detail.component';
+//import {DashboardComponent} from './dashboard.component';
+//import Map = require('esri/map');
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <a [routerLink]="['Dashboard']">Dashboard</a>
-    <a [routerLink]="['Heroes']">Heroes</a>
-    <router-outlet></router-outlet>
+    <h1>Hello world</h1>
+    <p>la map</p>
+    <div id="map"> </div>
   `,
   styles: [`
     a {padding: 5px;text-decoration: none;}
@@ -19,22 +18,22 @@ import Map = require('esri/map');
     a:hover {color: white; background-color: #1171a3;}
     a.router-link-active {color: white; background-color: #52b9e9;}
   `],
-  directives: [ROUTER_DIRECTIVES]
+  //directives: [ROUTER_DIRECTIVES]
 })
-@RouteConfig([
+//@RouteConfig([
   // {path: '/', redirectTo: ['Dashboard'] },
-  {path: '/', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
-  {path: '/heroes', name: 'Heroes', component: HeroesComponent},
-  {path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent}
-])
-export class AppComponent {
-  public title = 'Tour of Heroes';
-
+//  {path: '/', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
+//  {path: '/heroes', name: 'Heroes', component: HeroesComponent},
+//  {path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent}
+//])
+export class AppComponent { 
   constructor() {
-    var map : Map = new Map("map", {
+    require(["esri/map", "dojo/domReady!"], function(Map) {
+      var map = new Map("map", {
       center: [-118, 34.5],
       zoom: 8,
       basemap: "topo"
-    });
-  }
+      });
+    }
+  } 
 }
